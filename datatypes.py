3eae49386523bac4c014000ecda8824af137992c
vishalshep08 <vishalrameshshep0809@gmail.com>
2025-01-10 12:02:21 +0530

    Commit

--- datatypes.py
+++ datatypes.py
@@ -1,14 +1,36 @@
+#Identifying the various datatypes with the type function
 a=100
 b=20.10
 c=20+3j
-string='Hello World!'
+str='Hello World!'
 list=[10,20]
 tuple=(10,20)
 dict={'name':'Vishal','age':20}
 print("The datatype of the variable a is",type(a))
 print("The datatype of the variable b is",type(b))
 print("The datatype of the variable c is",type(c))
-print("The datatype of the variable string is",type(string))
+print("The datatype of the variable str is",type(str))
 print("The datatype of the variable list is",type(list))
 print("The datatype of the variable tuple is",type(tuple))
 print("The datatype of the variable dict is",type(dict))
+
+print(type(100))
+print(str)
+print(str[0:2])
+
+#List
+list1=[10,'Python',20.10]
+print(list1)
+print("The datatype of the variable list1 is",type(list1))
+#Tuple
+tuple1=(10,'Python',20.10)
+print(tuple1)
+print("The datatype of the variable tuple1 is",type(tuple1))
+#Set
+set1={10,20,30}
+print(set1)
+print("The datatype of the variable set1 is",type(set1))
+#Range
+for i in range(10):
+    print(i)
+
