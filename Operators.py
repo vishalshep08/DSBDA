e11d45a7e5972134819409606dad0abee0f8983a
vishalshep08 <vishalrameshshep0809@gmail.com>
2025-01-10 12:28:12 +0530

    Commit

---
+++ Operators.py
@@ -0,0 +1,22 @@
+#Arithematic Operators
+a=29
+b=10
+
+c=a+b
+print("The addition of two numbers is :",c)
+
+c=a-b
+print("The subtraction of two numbers is :",c)
+
+c=a*b
+print("The multiplication of two numbers is :",c)
+
+c=a/b
+print("The division of two numbers is :",c)
+
+c=a%b
+print("The remainder of two numbers is :",c)
+
+c=a**b
+print("The exponent of two numbers is :",c)
+
