4decdfc98ac2e929fd645abd91fa632a3a95364d
vishalshep08 <vishalrameshshep0809@gmail.com>
2025-01-10 11:36:09 +0530

    Commit

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+a=100
+b=20.10
+c=20+3j
+string='Hello World!'
+list=[10,20]
+tuple=(10,20)
+dict={'name':'Vishal','age':20}
+print("The datatype of the variable a is",type(a))
+print("The datatype of the variable b is",type(b))
+print("The datatype of the variable c is",type(c))
+print("The datatype of the variable string is",type(string))
+print("The datatype of the variable list is",type(list))
+print("The datatype of the variable tuple is",type(tuple))
+print("The datatype of the variable dict is",type(dict))
